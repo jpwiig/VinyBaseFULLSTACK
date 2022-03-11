1302defeb03c4a201bac61b86bcd7b012bf49545
jpwiig <jpwiig@gmail.com>
2022-03-11 21:10:03 +0100

    init

---
+++ src/main/java/com/example/vinylbasefullstack/VinylbaseFullstackApplication.java
@@ -0,0 +1,13 @@
+package com.example.vinylbasefullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinylbaseFullstackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VinylbaseFullstackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vinylbasefullstack/VinylbaseFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vinylbasefullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinylbaseFullstackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
